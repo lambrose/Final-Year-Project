4d1d95719817ee28b02350536beba55bfcdd7f39
lambrose <l.ambrose1@nuigalway.ie>
2019-10-27 14:46:41 +0000

    Flask web application skeleton

---
+++ loop/routes.py
@@ -0,0 +1,69 @@
+from flask import render_template, url_for, flash, redirect, request
+from loop import app, db, bcrypt
+from loop.forms import RegistrationForm, LoginForm
+from loop.models import Users
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+movies = [
+    {
+        'author': 'Louis',
+        'title': 'Movie name',
+        'content': 'Movie',
+        'date_posted': 'Oct 2019'
+    }
+]
+
+
+@app.route("/")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = Users(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('account'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
+
+@app.route("/group")
+@login_required
+def group():
+    return render_template('group.html', title='Group', movies=movies)
