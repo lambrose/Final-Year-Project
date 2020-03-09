9ceaf10c88754e17242ac162c81af6199cc33804
lambrose <l.ambrose1@nuigalway.ie>
2020-03-09 11:29:27 +0000

    Refactoring users file

--- loop/users/forms.py
+++ loop/users/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from loop.models import Users
 
 
+# Creating a registration form with the form fields
 class RegistrationForm(FlaskForm):
     first_name = StringField('First Name',
                              validators=[DataRequired(), Length(min=2, max=20)])
@@ -16,12 +17,14 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # This function ensures that the email in the db is unique
     def validate_email(self, email):
         user = Users.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
 
+# Login form for the login page
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
--- loop/users/routes.py
+++ loop/users/routes.py
@@ -9,13 +9,16 @@ users = Blueprint('users', __name__)
 
 @users.route("/register", methods=['GET', 'POST'])
 def register():
+    # ensure that if the user is logged in, then they cannot register another account
     if current_user.is_authenticated:
         return redirect(url_for('main.watch'))
     form = RegistrationForm()
+    # when the form has been filled and submitted, encypt the password before saving it in the database
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = Users(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data,
                      password=hashed_password)
+        # adding this entry to the db
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
@@ -26,13 +29,16 @@ def register():
 @users.route("/", methods=['GET', 'POST'])
 @users.route("/login", methods=['GET', 'POST'])
 def login():
+    # ensure that if the user is logged in, then they cannot login to another account
     if current_user.is_authenticated:
         return redirect(url_for('main.watch'))
     form = LoginForm()
+    # when the form has been filled and submitted, verify that the credentials are in the database
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
+            # if there was a page clicked before the login, it will be redirected to it after the login
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('main.watch'))
         else:
@@ -40,6 +46,7 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+# logout function
 @users.route("/logout")
 def logout():
     logout_user()
