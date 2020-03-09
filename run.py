ad9b34d86e0e53508deb7a5474cfbe3dd9833224
lambrose <l.ambrose1@nuigalway.ie>
2020-03-09 13:29:48 +0000

    Adding app configurations

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from loop import create_app
+from flask_talisman import Talisman
 
 app = create_app()
 
 if __name__ == '__main__':
+    # redirect to https
+    Talisman(app)
     app.run(debug=True)
